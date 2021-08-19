5355e832deae814ca17e6c0441f35c47aa63b5d0
Arushi Katyal <katy.arushi@gmail.com>
2021-08-19 01:05:07 -0400

    add move functionality to client code

--- client.js
+++ client.js
@@ -10,6 +10,14 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: AK');
+    // setTimeout(() => {
+    //   conn.write("Move: up"),
+    //   1000
+    // });
+    // setTimeout(() => {
+    //   conn.write("Move: left"),
+    //   1000
+    // });
   });
 
   conn.on('data', (data) => {
