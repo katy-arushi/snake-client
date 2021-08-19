5037dc96a78f08193e9be61941ee95c6d0984e98
Arushi Katyal <katy.arushi@gmail.com>
2021-08-19 01:18:03 -0400

    allow players to send keyboard commands using stdin

--- play.js
+++ play.js
@@ -2,3 +2,24 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
