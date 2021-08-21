77e0a7dc1748c38a26a19a0d6f092183fb7ce078
Arushi Katyal <katy.arushi@gmail.com>
2021-08-21 02:00:41 -0400

    add comment

--- play.js
+++ play.js
@@ -1,7 +1,9 @@
 const { connect } = require('./client');
 const { setUpInput } = require('./input');
 
+// Connecting to the server
 console.log('Connecting ...');
 const conn = connect();
 
+// for user inputs
 setUpInput(conn);
